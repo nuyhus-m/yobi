9f8e33c07b19bb4feae61b21f62929a36299ab0f
ghaud5684 <ghaud5684@naver.com>
2025-04-17 10:20:21 +0900

    Android

---
+++ Android/settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://git.onesoftdigm.dev/api/v4/projects/100/packages/maven") }
+    }
+}
+
+rootProject.name = "FitrusEstDeviceSDKDemo"
+include(":app")
+ 
