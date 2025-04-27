f2382f899c851871ad0134df6755d3c892c54fff
민수현 <minsh2344@gmail.com>
2025-04-28 01:39:13 +0900

    chore: gradle 파일 초기 설정 및 라이브러리 추가

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -1,6 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    // safeArgs
+    alias(libs.plugins.navigation.safe.args)
+
+    // hilt
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.dagger.hilt.android)
 }
 
 android {
@@ -46,4 +53,29 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // retrofit
+    // https://github.com/square/retrofit
+    implementation(libs.retrofit)
+    // https://github.com/square/okhttp
+    implementation(libs.okhttp)
+    // https://github.com/square/retrofit/tree/master/retrofit-converters/gson
+    implementation(libs.converter.gson)
+    // https://github.com/square/okhttp/tree/master/okhttp-logging-interceptor
+    implementation(libs.logging.interceptor)
+
+    // Glide
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+
+    //framework ktx dependency
+    implementation(libs.androidx.fragment.ktx)
+
+    // Jetpack Navigation Kotlin
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    // hilt
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.hilt.android)
 }
