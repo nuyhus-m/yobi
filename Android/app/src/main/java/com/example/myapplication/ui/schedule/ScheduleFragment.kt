981746ba59e861e3fd5e87f31fc338db1496057f
민수현 <minsh2344@gmail.com>
2025-05-06 00:22:22 +0900

    design: 일정 등록 DialogFragment 구현

--- Android/app/src/main/java/com/example/myapplication/ui/schedule/ScheduleFragment.kt
+++ Android/app/src/main/java/com/example/myapplication/ui/schedule/ScheduleFragment.kt
@@ -1,5 +1,8 @@
 package com.example.myapplication.ui.schedule
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 import com.example.myapplication.base.BaseFragment
 import com.example.myapplication.databinding.FragmentScheduleBinding
@@ -10,5 +13,11 @@ class ScheduleFragment : BaseFragment<FragmentScheduleBinding>(
     FragmentScheduleBinding::bind,
     R.layout.fragment_schedule
 ) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.tv.setOnClickListener {
+            findNavController().navigate(R.id.dest_schedule_register_dialog)
+        }
+    }
 }
