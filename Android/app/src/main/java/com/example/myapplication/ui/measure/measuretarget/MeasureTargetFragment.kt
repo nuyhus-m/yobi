991ede29f66dfd468cb582f590b97417d1142328
민수현 <minsh2344@gmail.com>
2025-05-07 00:04:47 +0900

    design: MeasureTargetFragment UI 구현

--- Android/app/src/main/java/com/example/myapplication/ui/measure/measuretarget/MeasureTargetFragment.kt
+++ Android/app/src/main/java/com/example/myapplication/ui/measure/measuretarget/MeasureTargetFragment.kt
@@ -1,5 +1,9 @@
 package com.example.myapplication.ui.measure.measuretarget
 
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.example.myapplication.R
 import com.example.myapplication.base.BaseFragment
 import com.example.myapplication.databinding.FragmentMeasureTargetBinding
@@ -11,4 +15,36 @@ class MeasureTargetFragment : BaseFragment<FragmentMeasureTargetBinding>(
     R.layout.fragment_measure_target
 ) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupSpinner()
+    }
+
+    private fun setupSpinner() {
+        val tempStrings = listOf("이서현", "이호정", "박진현")
+
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.item_spinner,
+            tempStrings
+        )
+
+        adapter.setDropDownViewResource(R.layout.item_spinner_dropdown)
+        binding.spinner.adapter = adapter
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selected = parent.getItemAtPosition(position).toString()
+                // TODO 측정 대상 선택
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+    }
 }
