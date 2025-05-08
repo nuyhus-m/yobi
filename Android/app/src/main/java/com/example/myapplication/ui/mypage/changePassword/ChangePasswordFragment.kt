8cffc6178488148532627886d76337b46b79b05e
박진현 <pjiy1220@naver.com>
2025-05-09 01:02:03 +0900

    feat: 비밀번호 변경 완료, 뒤로가기

--- Android/app/src/main/java/com/example/myapplication/ui/mypage/changePassword/ChangePasswordFragment.kt
+++ Android/app/src/main/java/com/example/myapplication/ui/mypage/changePassword/ChangePasswordFragment.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.ui.mypage.changePassword
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 import com.example.myapplication.base.BaseFragment
 import com.example.myapplication.databinding.FragmentChangePasswordBinding
@@ -14,6 +15,15 @@ class ChangePasswordFragment: BaseFragment<FragmentChangePasswordBinding>(
 ){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnDone.setOnClickListener {
+            showToast("비밀번호가 변경되었습니다.")
+            findNavController().popBackStack()
+        }
+
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
 
     }
 }
