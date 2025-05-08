dbfd2944312f2d03aafd2b65319a32edf7ac4238
민수현 <minsh2344@gmail.com>
2025-05-08 17:25:00 +0900

    design: MeasureLoadingFragment UI 구현

---
+++ Android/app/src/main/java/com/example/myapplication/ui/measure/measureloading/MeasureLoadingFragment.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.ui.measure.measureloading
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.R
+import com.example.myapplication.base.BaseFragment
+import com.example.myapplication.databinding.FragmentMeasureLoadingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MeasureLoadingFragment : BaseFragment<FragmentMeasureLoadingBinding>(
+    FragmentMeasureLoadingBinding::bind,
+    R.layout.fragment_measure_loading
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+}
