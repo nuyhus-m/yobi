5565c1db60194b9ab1a63b4af07d58aac18a1499
박진현 <pjiy1220@naver.com>
2025-05-09 09:29:38 +0900

    feat: 스플래시 액티비티 생성

--- Android/app/src/main/java/com/example/myapplication/ui/SplashActivity.kt
+++ Android/app/src/main/java/com/example/myapplication/ui/SplashActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,13 +11,19 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
 import com.example.myapplication.base.BaseActivity
 import com.example.myapplication.databinding.ActivityAuthBinding
+import com.example.myapplication.databinding.ActivitySplashBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AuthActivity : BaseActivity<ActivityAuthBinding>(ActivityAuthBinding::inflate) {
+class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // 2초 후 MainActivity로 이동
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }, 2000)
 
     }
 }
