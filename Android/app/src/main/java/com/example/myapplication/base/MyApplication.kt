9e5553604d389c4be0091b2ee5226040aa3c1352
민수현 <minsh2344@gmail.com>
2025-05-12 11:25:52 +0900

    chore: 다크모드 비활성화

--- Android/app/src/main/java/com/example/myapplication/base/MyApplication.kt
+++ Android/app/src/main/java/com/example/myapplication/base/MyApplication.kt
@@ -1,10 +1,17 @@
 package com.example.myapplication.base
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // 다크모드 비활성화
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 
     companion object {
         const val BASE_URL = "http://k12s209.p.ssafy.io:8081/api/"
