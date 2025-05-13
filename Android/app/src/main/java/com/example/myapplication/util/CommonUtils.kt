215700c074e69a7e6dde5c9bf1a35e28c8b1550c
이호정 <ghaud5684@naver.com>
2025-05-13 17:07:23 +0900

    feat

--- Android/app/src/main/java/com/example/myapplication/util/CommonUtils.kt
+++ Android/app/src/main/java/com/example/myapplication/util/CommonUtils.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication.util
 
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
 object CommonUtils {
 
-//    //날짜 포맷 출력
-//    fun dateformatYMDHM(time: Date):String{
-//        val format = SimpleDateFormat("yyyy.MM.dd. HH:mm", Locale.KOREA)
-//        format.timeZone = TimeZone.getTimeZone("Asia/Seoul")
-//        return format.format(time)
-//    }
+    //날짜 포맷 출력
+    fun dateformatYMDHM(time: Date):String{
+        val format = SimpleDateFormat("yyyy.MM.dd. HH:mm", Locale.KOREA)
+        format.timeZone = TimeZone.getTimeZone("Asia/Seoul")
+        return format.format(time)
+    }
 
 }
