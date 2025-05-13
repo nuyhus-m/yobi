0213ba41cc95df192604f7706f0635dc5924a81a
민수현 <minsh2344@gmail.com>
2025-05-13 21:59:39 +0900

    feat: 측정 결과 조회 관련 api 연동 S12P31S209-142

--- Android/app/src/main/java/com/example/myapplication/data/remote/MeasureService.kt
+++ Android/app/src/main/java/com/example/myapplication/data/remote/MeasureService.kt
@@ -6,12 +6,24 @@ import com.example.myapplication.data.dto.request.measure.HeartRateRequest
 import com.example.myapplication.data.dto.request.measure.RequiredDataRequest
 import com.example.myapplication.data.dto.request.measure.StressRequest
 import com.example.myapplication.data.dto.request.measure.TemperatureRequest
+import com.example.myapplication.data.dto.response.measure.BloodPressureResponse
+import com.example.myapplication.data.dto.response.measure.BloodPressureResultResponse
+import com.example.myapplication.data.dto.response.measure.BodyCompositionResponse
+import com.example.myapplication.data.dto.response.measure.BodyCompositionResultResponse
+import com.example.myapplication.data.dto.response.measure.HeartRateResponse
+import com.example.myapplication.data.dto.response.measure.HeartRateResultResponse
+import com.example.myapplication.data.dto.response.measure.RequiredDataResponse
 import com.example.myapplication.data.dto.response.measure.RequiredStatusResponse
+import com.example.myapplication.data.dto.response.measure.StressResponse
+import com.example.myapplication.data.dto.response.measure.StressResultResponse
+import com.example.myapplication.data.dto.response.measure.TemperatureResponse
+import com.example.myapplication.data.dto.response.measure.TemperatureResultResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MeasureService {
 
@@ -26,40 +38,70 @@ interface MeasureService {
     suspend fun saveRequiredMeasureData(
         @Path("clientId") clientId: Int,
         @Body body: RequiredDataRequest
-    ): Response<Unit>
+    ): Response<RequiredDataResponse>
 
     // 체성분 데이터 저장
     @POST("health/body/{clientId}")
     suspend fun saveBodyCompositionData(
         @Path("clientId") clientId: Int,
         @Body body: BodyCompositionRequest
-    ): Response<Unit>
+    ): Response<BodyCompositionResponse>
 
-    // 심박 측정 데이터 저장
+    // 심박 데이터 저장
     @POST("health/heart-rate/{clientId}")
     suspend fun saveHeartRateData(
         @Path("clientId") clientId: Int,
         @Body body: HeartRateRequest
-    ): Response<Unit>
+    ): Response<HeartRateResponse>
 
     // 혈압 데이터 저장
     @POST("health/blood-pressure/{clientId}")
     suspend fun saveBloodPressureData(
         @Path("clientId") clientId: Int,
         @Body body: BloodPressureRequest
-    ): Response<Unit>
+    ): Response<BloodPressureResponse>
 
     // 스트레스 데이터 저장
     @POST("health/stress/{clientId}")
     suspend fun saveStressData(
         @Path("clientId") clientId: Int,
         @Body body: StressRequest
-    ): Response<Unit>
+    ): Response<StressResponse>
 
     // 체온 데이터 저장
     @POST("health/temperature/{clientId}")
     suspend fun saveTemperatureData(
         @Path("clientId") clientId: Int,
         @Body body: TemperatureRequest
-    ): Response<Unit>
+    ): Response<TemperatureResponse>
+
+    // 체성분 결과 조회
+    @GET("health/body")
+    suspend fun getBodyCompResult(
+        @Query("bodyId") bodyId: Int
+    ): Response<BodyCompositionResultResponse>
+
+    // 혈압 결과 조회
+    @GET("health/blood")
+    suspend fun getBloodPressureResult(
+        @Query("bloodId") bloodId: Int
+    ): Response<BloodPressureResultResponse>
+
+    // 심박 결과 조회
+    @GET("health/heartRate")
+    suspend fun getHeartRateResult(
+        @Query("heartRateId") heartRateId: Int
+    ): Response<HeartRateResultResponse>
+
+    // 스트레스 결과 조회
+    @GET("health/stress")
+    suspend fun getStressResult(
+        @Query("stressId") stressId: Int
+    ): Response<StressResultResponse>
+
+    // 체온 결과 조회
+    @GET("health/temperature")
+    suspend fun getTemperatureResult(
+        @Query("temperatureId") temperatureId: Int
+    ): Response<TemperatureResultResponse>
 }
--- Android/app/src/main/java/com/example/myapplication/data/repository/MeasureRepository.kt
+++ Android/app/src/main/java/com/example/myapplication/data/repository/MeasureRepository.kt
@@ -6,7 +6,18 @@ import com.example.myapplication.data.dto.request.measure.HeartRateRequest
 import com.example.myapplication.data.dto.request.measure.RequiredDataRequest
 import com.example.myapplication.data.dto.request.measure.StressRequest
 import com.example.myapplication.data.dto.request.measure.TemperatureRequest
+import com.example.myapplication.data.dto.response.measure.BloodPressureResponse
+import com.example.myapplication.data.dto.response.measure.BloodPressureResultResponse
+import com.example.myapplication.data.dto.response.measure.BodyCompositionResponse
+import com.example.myapplication.data.dto.response.measure.BodyCompositionResultResponse
+import com.example.myapplication.data.dto.response.measure.HeartRateResponse
+import com.example.myapplication.data.dto.response.measure.HeartRateResultResponse
+import com.example.myapplication.data.dto.response.measure.RequiredDataResponse
 import com.example.myapplication.data.dto.response.measure.RequiredStatusResponse
+import com.example.myapplication.data.dto.response.measure.StressResponse
+import com.example.myapplication.data.dto.response.measure.StressResultResponse
+import com.example.myapplication.data.dto.response.measure.TemperatureResponse
+import com.example.myapplication.data.dto.response.measure.TemperatureResultResponse
 import com.example.myapplication.data.remote.MeasureService
 import retrofit2.Response
 import javax.inject.Inject
@@ -22,36 +33,59 @@ class MeasureRepository @Inject constructor(
     suspend fun saveRequiredMeasureData(
         clientId: Int,
         body: RequiredDataRequest
-    ): Response<Unit> {
+    ): Response<RequiredDataResponse> {
         return measureService.saveRequiredMeasureData(clientId, body)
     }
 
     suspend fun saveBodyCompositionData(
         clientId: Int,
         body: BodyCompositionRequest
-    ): Response<Unit> {
+    ): Response<BodyCompositionResponse> {
         return measureService.saveBodyCompositionData(clientId, body)
     }
 
-    suspend fun saveHeartRateData(clientId: Int, body: HeartRateRequest): Response<Unit> {
+    suspend fun saveHeartRateData(
+        clientId: Int,
+        body: HeartRateRequest
+    ): Response<HeartRateResponse> {
         return measureService.saveHeartRateData(clientId, body)
     }
 
     suspend fun saveBloodPressureData(
         clientId: Int,
         body: BloodPressureRequest
-    ): Response<Unit> {
+    ): Response<BloodPressureResponse> {
         return measureService.saveBloodPressureData(clientId, body)
     }
 
-    suspend fun saveStressData(clientId: Int, body: StressRequest): Response<Unit> {
+    suspend fun saveStressData(clientId: Int, body: StressRequest): Response<StressResponse> {
         return measureService.saveStressData(clientId, body)
     }
 
     suspend fun saveTemperatureData(
         clientId: Int,
         body: TemperatureRequest
-    ): Response<Unit> {
+    ): Response<TemperatureResponse> {
         return measureService.saveTemperatureData(clientId, body)
+    }
+
+    suspend fun getBodyCompResult(bodyId: Int): Response<BodyCompositionResultResponse> {
+        return measureService.getBodyCompResult(bodyId)
+    }
+
+    suspend fun getBloodPressureResult(bloodId: Int): Response<BloodPressureResultResponse> {
+        return measureService.getBloodPressureResult(bloodId)
+    }
+
+    suspend fun getHeartRateResult(heartRateId: Int): Response<HeartRateResultResponse> {
+        return measureService.getHeartRateResult(heartRateId)
+    }
+
+    suspend fun getStressResult(stressId: Int): Response<StressResultResponse> {
+        return measureService.getStressResult(stressId)
+    }
+
+    suspend fun getTemperatureResult(temperatureId: Int): Response<TemperatureResultResponse> {
+        return measureService.getTemperatureResult(temperatureId)
     }
 }
