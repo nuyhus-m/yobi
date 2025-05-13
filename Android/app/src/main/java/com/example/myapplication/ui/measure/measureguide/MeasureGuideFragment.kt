cefc82867d6d68dcc5f67892f03e2304ed995edb
민수현 <minsh2344@gmail.com>
2025-05-13 11:21:23 +0900

    design: measureType에 따라서 MeasureGuideFragment View 초기화

--- Android/app/src/main/java/com/example/myapplication/ui/measure/measureguide/MeasureGuideFragment.kt
+++ Android/app/src/main/java/com/example/myapplication/ui/measure/measureguide/MeasureGuideFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 import com.example.myapplication.base.BaseFragment
+import com.example.myapplication.base.HealthDataType
 import com.example.myapplication.databinding.FragmentMeasureGuideBinding
 import com.example.myapplication.ui.FitrusViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,12 +22,37 @@ class MeasureGuideFragment : BaseFragment<FragmentMeasureGuideBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setTitle()
         initButton()
+        initViews()
+    }
+
+    private fun setTitle() {
+        binding.tvTitle.text = getString(R.string.measure_title, fitrusViewModel.clientName)
     }
 
     private fun initButton() {
         binding.ivBack.setOnClickListener {
             findNavController().popBackStack()
+        }
+    }
+
+    private fun initViews() {
+        when (fitrusViewModel.measureType) {
+            HealthDataType.BODY_COMPOSITION -> {
+                binding.groupGrip.visibility = View.VISIBLE
+                binding.tvGuide.text = getString(R.string.measure_grip_guide)
+            }
+
+            HealthDataType.HEART_RATE, HealthDataType.BLOOD_PRESSURE, HealthDataType.STRESS -> {
+                binding.ivFinger.visibility = View.VISIBLE
+                binding.tvGuide.text = getString(R.string.measure_finger_touch_guide)
+            }
+
+            HealthDataType.TEMPERATURE -> {
+                binding.ivForehead.visibility = View.VISIBLE
+                binding.tvGuide.text = getString(R.string.measure_forehead_touch_guide)
+            }
         }
     }
 
