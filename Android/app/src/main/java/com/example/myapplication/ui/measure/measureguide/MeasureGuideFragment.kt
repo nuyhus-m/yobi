d6d4336033b5e63a1eaf5f75214890a7f3f60d3e
민수현 <minsh2344@gmail.com>
2025-05-13 10:01:27 +0900

    feat: 연결 가이드, 측정 가이드 화면 이동 구현

--- Android/app/src/main/java/com/example/myapplication/ui/measure/measureguide/MeasureGuideFragment.kt
+++ Android/app/src/main/java/com/example/myapplication/ui/measure/measureguide/MeasureGuideFragment.kt
@@ -2,10 +2,12 @@ package com.example.myapplication.ui.measure.measureguide
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 import com.example.myapplication.base.BaseFragment
 import com.example.myapplication.databinding.FragmentMeasureGuideBinding
+import com.example.myapplication.ui.FitrusViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,11 +16,22 @@ class MeasureGuideFragment : BaseFragment<FragmentMeasureGuideBinding>(
     R.layout.fragment_measure_guide
 ) {
 
+    private val fitrusViewModel by activityViewModels<FitrusViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initButton()
+    }
+
+    private fun initButton() {
         binding.ivBack.setOnClickListener {
             findNavController().popBackStack()
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        fitrusViewModel.disconnectDevice()
     }
 }
