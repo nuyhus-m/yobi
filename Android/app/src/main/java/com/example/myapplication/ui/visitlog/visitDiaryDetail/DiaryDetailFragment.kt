7e0d895b8d43e462463f5cdfecee230f9f3227a5
이호정 <ghaud5684@naver.com>
2025-05-13 10:49:54 +0900

    feat : diaryDetail UI 생성

---
+++ Android/app/src/main/java/com/example/myapplication/ui/visitlog/visitDiaryDetail/DiaryDetailFragment.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.ui.visitlog.visitDiaryDetail
+
+import com.example.myapplication.R
+import com.example.myapplication.base.BaseFragment
+import com.example.myapplication.databinding.FragmentDiaryDetailBinding
+
+class DiaryDetailFragment : BaseFragment<FragmentDiaryDetailBinding>(
+    FragmentDiaryDetailBinding::bind,
+    R.layout.fragment_diary_detail
+
+) {
+
+}
