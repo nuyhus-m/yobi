9975c864a787613657ee5da2c923054e6189098f
박진현 <pjiy1220@naver.com>
2025-05-14 15:35:44 +0900

    feat: 유저 정보 MainViewModel에 추가

--- Android/app/src/main/java/com/example/myapplication/data/remote/UserService.kt
+++ Android/app/src/main/java/com/example/myapplication/data/remote/UserService.kt
@@ -1,6 +1,13 @@
 package com.example.myapplication.data.remote
 
+import com.example.myapplication.data.dto.response.UserResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface UserService {
+
+    @GET("users/")
+    suspend fun getMyInfo(): Response<UserResponse>
 
 //    // 사용자 정보를 추가한다.
 //    @POST("rest/user")
