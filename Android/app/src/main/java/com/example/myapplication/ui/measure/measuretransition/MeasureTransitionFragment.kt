798e7a6dda9a4735963e767a482a2f45d1fc50ac
민수현 <minsh2344@gmail.com>
2025-05-15 17:04:14 +0900

    design: MeasureTransitionFragment UI 구현

---
+++ Android/app/src/main/java/com/example/myapplication/ui/measure/measuretransition/MeasureTransitionFragment.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication.ui.measure.measuretransition
+
+import com.example.myapplication.R
+import com.example.myapplication.base.BaseFragment
+import com.example.myapplication.databinding.FragmentMeasureTransitionBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MeasureTransitionFragment : BaseFragment<FragmentMeasureTransitionBinding>(
+    FragmentMeasureTransitionBinding::bind,
+    R.layout.fragment_measure_transition
+) {
+
+}
