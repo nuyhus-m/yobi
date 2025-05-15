64ea66f6cd2495bb271178cbeb73c0b484b467b7
민수현 <minsh2344@gmail.com>
2025-05-15 09:23:44 +0900

    feat: 측정 항목 선택 기능 구현

--- Android/app/src/main/java/com/example/myapplication/ui/measure/measureitem/MeasureItemFragment.kt
+++ Android/app/src/main/java/com/example/myapplication/ui/measure/measureitem/MeasureItemFragment.kt
@@ -2,10 +2,13 @@ package com.example.myapplication.ui.measure.measureitem
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 import com.example.myapplication.base.BaseFragment
+import com.example.myapplication.base.HealthDataType
 import com.example.myapplication.databinding.FragmentMeasureItemBinding
+import com.example.myapplication.ui.FitrusViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,6 +16,8 @@ class MeasureItemFragment : BaseFragment<FragmentMeasureItemBinding>(
     FragmentMeasureItemBinding::bind,
     R.layout.fragment_measure_item
 ) {
+
+    private val fitrusViewModel by activityViewModels<FitrusViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,14 +27,22 @@ class MeasureItemFragment : BaseFragment<FragmentMeasureItemBinding>(
         }
 
         binding.tvBodyComp.setOnClickListener {
+            fitrusViewModel.setMeasureType(HealthDataType.BODY_COMPOSITION)
             findNavController().navigate(R.id.dest_measure_guide)
         }
 
         binding.tvHeartRate.setOnClickListener {
+            fitrusViewModel.setMeasureType(HealthDataType.HEART_RATE)
             findNavController().navigate(R.id.dest_device_connect)
         }
 
         binding.tvBloodPressure.setOnClickListener {
+            fitrusViewModel.setMeasureType(HealthDataType.BLOOD_PRESSURE)
+            findNavController().navigate(R.id.dest_measure_loading)
+        }
+
+        binding.tvStress.setOnClickListener {
+            fitrusViewModel.setMeasureType(HealthDataType.STRESS)
             findNavController().navigate(R.id.dest_measure_loading)
         }
 
