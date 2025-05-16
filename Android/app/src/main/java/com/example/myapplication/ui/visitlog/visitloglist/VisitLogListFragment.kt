161800e4b829231709f50a2c060ff5b26c73d265
이호정 <ghaud5684@naver.com>
2025-05-16 10:30:59 +0900

    fix : emptyList() 해ê² 완료!

--- Android/app/src/main/java/com/example/myapplication/ui/visitlog/visitloglist/VisitLogListFragment.kt
+++ Android/app/src/main/java/com/example/myapplication/ui/visitlog/visitloglist/VisitLogListFragment.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 import com.example.myapplication.base.BaseFragment
 import com.example.myapplication.databinding.FragmentVisitLogListBinding
+import com.example.myapplication.ui.visitlog.diarydetail.DiaryDetailFragment
 import com.example.myapplication.ui.visitlog.visitloglist.adapter.FilterAdapter
 import com.example.myapplication.ui.visitlog.visitloglist.adapter.VisitLogAdapter
 import com.example.myapplication.ui.visitlog.visitloglist.viewmodel.VisitLogViewModel
@@ -20,7 +21,6 @@ class VisitLogListFragment : BaseFragment<FragmentVisitLogListBinding>(
     R.layout.fragment_visit_log_list
 ) {
     private val viewModel: VisitLogViewModel by viewModels()
-    private val REFRESH_KEY = "refresh_logs"
 
     private val logAdapter = VisitLogAdapter { selectedLog ->
         val action = VisitLogListFragmentDirections
@@ -32,28 +32,6 @@ class VisitLogListFragment : BaseFragment<FragmentVisitLogListBinding>(
 
     private val filterAdapter = FilterAdapter { selected ->
         viewModel.selectFilter(selected)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // 새로고침 플래그 관찰 설정
-        findNavController().currentBackStackEntry
-            ?.savedStateHandle
-            ?.getLiveData<Boolean>(
-                REFRESH_KEY
-            )
-            ?.observe(this) { shouldRefresh ->
-                if (shouldRefresh == true) {
-                    // 서버에서 최신 데이터 강제로 다시 로드
-                    viewModel.forceRefresh()
-                    // 플래그 초기화
-                    findNavController().currentBackStackEntry?.savedStateHandle?.set(
-                        REFRESH_KEY,
-                        false
-                    )
-                }
-            }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -73,5 +51,11 @@ class VisitLogListFragment : BaseFragment<FragmentVisitLogListBinding>(
         viewModel.filteredLogs.observe(viewLifecycleOwner) {
             logAdapter.submitList(it)
         }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.forceRefresh()
     }
 }
