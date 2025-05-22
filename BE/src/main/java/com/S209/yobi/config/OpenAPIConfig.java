84b84418658ad2063e3695818975b07a2ff737f5
이문경 <moon9433@naver.com>
2025-05-22 12:04:26 +0900

    Update BE/src/main/java/com/S209/yobi/config/OpenAPIConfig.java
    Former-commit-id: 4d8fec2a11e36e33cf8a0bcea81edf0fb928d90d

--- BE/src/main/java/com/S209/yobi/config/OpenAPIConfig.java
+++ BE/src/main/java/com/S209/yobi/config/OpenAPIConfig.java
@@ -23,13 +23,13 @@ public class OpenAPIConfig {
                         .description("Yobi 서비스의 API 명세서입니다.")
                         .version("v1.0.0"))
                 .servers(List.of(
-                        new Server().url("http://k12s209.p.ssafy.io:8081/api").description("EC2 Server"),
-                        new Server().url("http://192.168.100.162:8080/api").description("서현 Server"),
-                        new Server().url("http://192.168.35.21:8080/api").description("서현 집"),
-                        new Server().url("http://192.168.100.201:8080/api").description("문경 Server"),
-                        new Server().url("http://192.168.0.115:8080/api").description("문경 집 Server"),
-                        new Server().url("http://172.30.1.71:8080/api").description("투썸 Server"),
-                        new Server().url("http://192.168.0.31:8080/api").description("에바 Server")
+                        new Server().url("https://k12s209.p.ssafy.io:8081/api").description("EC2 Server"),
+                        new Server().url("https://192.168.100.162:8080/api").description("서현 Server"),
+                        new Server().url("https://192.168.35.21:8080/api").description("서현 집"),
+                        new Server().url("https://192.168.100.201:8080/api").description("문경 Server"),
+                        new Server().url("https://192.168.0.115:8080/api").description("문경 집 Server"),
+                        new Server().url("https://172.30.1.71:8080/api").description("투썸 Server"),
+                        new Server().url("https://192.168.0.31:8080/api").description("에바 Server")
                 ))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new io.swagger.v3.oas.models.Components()
